e2900aab3be88e54d08a8c54bb295f3ac86aaec8
Nikhil Dharmaram <nikhil.dharmaram@gmail.com>
2026-01-13 20:11:59 -0600

    Prediction Pipeline and UI

--- application.py
+++ application.py
@@ -38,4 +38,4 @@ def predict_datapoint():
         return render_template('home.html', results = results[0])
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0") # can pass port = 5001 etc.. to the arguments      
+    app.run(host = "0.0.0.0", debug = True)       
